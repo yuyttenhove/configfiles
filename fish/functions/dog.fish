3ec90c6fbaf481cc310823b341ed68ae2cc0d2e2
Yolan Uyttenhove <yolan.uyttenhove@yazzoom.com>
2020-01-31 14:01:53 +0100

    added dog function (cat but with syntax highlighting

---
+++ fish/functions/dog.fish
@@ -0,0 +1,12 @@
+function _dog
+    for f in $argv
+        echo "Displaying contents of \"$f\":"
+        echo ""
+        highlight -O ansi $f --force
+        echo ""
+    end
+end
+
+function dog
+    _dog $argv | less -R
+end
